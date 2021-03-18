09c8eae512c2c90e75e15c490c5e2b728c0c8f4f
David Ulrich <david.ulrich4@gmail.com>
2021-03-18 20:18:34 +0100

    Create gallery  page template

--- page-templates/gallery.php
+++ page-templates/gallery.php
@@ -4,40 +4,11 @@
  */
 
 get_header();
-
-// echo '<pre>';
-// print_r( get_field('gallery') );
-// echo '</pre>';
-// die();
-
-//ACF variables
-$images = get_field('gallery');
 ?>
 
-<section class="container container-margin">
-    <div class="row">
-        <h1 class="site-title"><?php echo get_the_title( get_the_ID() );?></h1>
-    </div>
+    <h1 class="content__title"><?php echo get_the_title( get_the_ID() );?></h1>
 
-    <?php if( $images) :?>
-
-        <div class="row spotlight-group" data-control="fullscreen,page">
-        
-        <?php foreach( $images as $image) : ?>
-            <div class="col-md-4">
-                <div class="card border-0 rounded-0">
-                    <a href="<?php echo esc_url( $image['url'] ); ?>" class="spotlight">
-                        <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" class="card-img">
-                    </a>
-                </div>
-            </div>
-        <?php endforeach; ?>
-
-        </div>
-
-    <?php endif; ?>
-
-</section>
+    <?php echo display_images_from_folder(get_field('gallery_name')); ?>
 
 <?php 
 get_footer();
