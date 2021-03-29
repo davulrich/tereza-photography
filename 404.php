a5aaa0962d5755f60219fbbbb1c8f44fe520b9f7
David Ulrich <david.ulrich4@gmail.com>
2021-03-29 22:01:26 +0200

    404 page template and styles

--- 404.php
+++ 404.php
@@ -9,52 +9,15 @@
 
 get_header();
 ?>
+		<h1 class="content__title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'terezaphotography' ); ?></h1>
+		<section class="error-404">
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+			<span class="error-404__status-code">404</span>
+			<p class="error-404__text">The page you're after no longer exists here. Check out <a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="error-404__link">the homepage</a> instead, it's usually nicer than this page.</p>
 
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'terezaphotography' ); ?></h1>
-				</header><!-- .page-header -->
 
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'terezaphotography' ); ?></p>
 
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'terezaphotography' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$terezaphotography_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'terezaphotography' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$terezaphotography_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+		</section><!-- .error-404 -->
 
 <?php
 get_footer();
